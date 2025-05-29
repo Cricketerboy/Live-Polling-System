30136ebf9570ae73731e7b5a10bb83677873854f
Mohit Anand <praveenanand801@gmail.com>
2025-05-30 00:15:23 +0530

    chat history

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -1,6 +1,7 @@
 const { handleNewPoll, handleSubmitAnswer, handleDisconnect } = require('../controllers/poll.controller');
 
 let ioInstance;
+const messages = []; // <-- Store chat messages here
 
 
 const initializeSocket = (io) => {
@@ -8,13 +9,18 @@ const initializeSocket = (io) => {
 
   io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
-
+     // Send full chat history to newly connected client
+     socket.on('get-chat-history', () => {
+      socket.emit('chat-history', messages);
+    });
     socket.on('new-poll', (poll) => handleNewPoll(io, poll));
     socket.on('submit-answer', (data) => handleSubmitAnswer(io, data));
     socket.on('disconnect', () => handleDisconnect(socket));
     socket.on('send-message', ({ sender, message }) => {
-  io.emit('receive-message', { sender, message, timestamp: Date.now() });
-});
+      const newMessage = { sender, message, timestamp: Date.now() };
+      messages.push(newMessage);
+      io.emit('receive-message', newMessage);
+    });
 
   });
 };
