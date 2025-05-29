3853a853e47afa4e762f5dc371c494e25d257fbd
Mohit Anand <praveenanand801@gmail.com>
2025-05-29 20:08:32 +0530

    add participants

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -1,7 +1,7 @@
 const { handleNewPoll, handleSubmitAnswer, handleDisconnect } = require('../controllers/poll.controller');
 
 let ioInstance;
-const participants = new Map();
+
 
 const initializeSocket = (io) => {
   ioInstance = io;
@@ -9,24 +9,12 @@ const initializeSocket = (io) => {
   io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
-     socket.on('join', ({ name }) => {
-      participants.set(socket.id, name);
-      io.emit('participants', Array.from(participants.values())); // broadcast updated list
-    });
-
     socket.on('new-poll', (poll) => handleNewPoll(io, poll));
     socket.on('submit-answer', (data) => handleSubmitAnswer(io, data));
     socket.on('disconnect', () => handleDisconnect(socket));
     socket.on('send-message', ({ sender, message }) => {
   io.emit('receive-message', { sender, message, timestamp: Date.now() });
 });
-
-socket.on('disconnect', () => {
-      console.log('User disconnected:', socket.id);
-      participants.delete(socket.id); // Remove from list
-      io.emit('participants', Array.from(participants.values())); // broadcast updated list
-      handleDisconnect(socket);
-    });
 
   });
 };
